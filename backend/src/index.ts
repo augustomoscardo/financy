5dfbff11c20de46e3f00b915a2b52993fe41d102
Augusto Moscardo <augustomsaluz@gmail.com>
2025-12-17 13:28:20 -0300

    setting up project

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server"
+import express from "express"
+import { expressMiddleware } from "@as-integrations/express5"
+import { buildSchema } from "type-graphql"
+import cors from "cors"
+
+const PORT = 4444
+
+async function bootStrap() {
+  const schema = await buildSchema({
+    resolvers: [],
+    validate: false,
+    emitSchemaFile: "./schema.graphql"
+  })
+
+  const server = new ApolloServer({ schema })
+  await server.start()
+
+  const app = express()
+  app.use(express.json())
+  app.use(cors())
+
+  app.use("/graphql", expressMiddleware(server))
+
+  app.listen(PORT, () => console.log(`Server is running on localhost:${PORT}`))
+}
+
+bootStrap()
+
+
+
+
