60ef643fd343676cf3b0452c63219676108ce2df
Augusto Moscardo <augustomsaluz@gmail.com>
2026-01-07 12:41:02 -0300

    chore: configure project & install libs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,7 +9,13 @@ export default defineConfig({
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
+
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
+  },
 })
