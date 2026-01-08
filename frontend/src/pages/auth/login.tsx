b4ed89991ce59f38c3582fb1afac6cd06a68ddcc
Augusto Moscardo <augustomsaluz@gmail.com>
2026-01-08 13:44:24 -0300

    feat(login): structured login page

---
+++ frontend/src/pages/auth/login.tsx
@@ -0,0 +1,127 @@
+import logoImg from "@/assets/logo.svg"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { z } from "zod"
+import { Controller, useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Field, FieldLabel } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { EyeClosed, Lock, Mail, UserRoundPlus } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { Link } from "react-router-dom"
+import { Separator } from "@/components/ui/separator"
+
+const loginFormSchema = z.object({
+  email: z.email("Email inválido"),
+  password: z.string().min(6, "A senha deve ter no mínimo 6 caracteres"),
+})
+
+type LoginFormData = z.infer<typeof loginFormSchema>
+
+export function Login() {
+  const form = useForm<LoginFormData>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  })
+
+  async function handleLogin(data: LoginFormData) {
+    console.log(data);
+
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-8">
+      <img src={logoImg} alt="Logo" />
+
+      <Card className="max-w-md w-full p-8">
+        <CardHeader className="flex flex-col gap-1 justify-center p-0">
+          <CardTitle className="text-center font-bold text-xl text-gray-800 leading-7">Fazer login</CardTitle>
+          <CardDescription className="text-center text-gray-600 leading-6">Entre na sua conta para continuar</CardDescription>
+        </CardHeader>
+        <CardContent className="p-0">
+          <form onSubmit={form.handleSubmit(handleLogin)} className="flex flex-col gap-4">
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field }) => {
+                return (
+                  <Field>
+                    <FieldLabel htmlFor={field.name} className="text-gray-700">Email</FieldLabel>
+                    <div className="flex items-center gap-3 border border-gray-300 rounded-md py-[14px] px-3">
+                      <Mail size={16} className="text-gray-400" />
+                      <Input
+                        {...field}
+                        id={field.name}
+                        placeholder="mail@example.com"
+                        className="border-none outline-none focus:ring-0 focus-visible:ring-0 p-0 shadow-none placeholder:text-gray-400 rounded-none h-4 leading-[18px]"
+                      />
+                    </div>
+                  </Field>
+                )
+              }}
+            />
+
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field }) => {
+                return (
+                  <Field>
+                    <FieldLabel htmlFor={field.name} className="text-gray-700">Password</FieldLabel>
+                    <div className="flex items-center gap-3 border border-gray-300 rounded-md py-[14px] px-3">
+                      <Lock size={16} className="text-gray-400" />
+                      <Input
+                        {...field}
+                        id={field.name}
+                        type="password"
+                        placeholder="Digite sua senha"
+                        className="border-none outline-none focus:ring-0 focus-visible:ring-0 p-0 shadow-none placeholder:text-gray-400 rounded-none h-4 leading-[18px]"
+                      />
+                      <Button type="button" variant="ghost" className="p-0 hover:bg-transparent text-gray-400 hover:text-gray-700 h-4">
+                        <EyeClosed size={16} className="" />
+                      </Button>
+                    </div>
+                  </Field>
+                )
+              }}
+            />
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <Input id="remind-me" type="checkbox" className="w-4 h-4 cursor-pointer" />
+                <Label htmlFor="remind-me" className="text-gray-700 text-sm cursor-pointer leading-5">Lembrar-me</Label>
+              </div>
+              <Link to="#" className="text-sm text-brand-base text-decoration-none hover:underline hover:text-brand-dark leading-5">
+                Recuperar senha
+              </Link>
+            </div>
+
+            <Button className="bg-brand-base text-white rounded-md p-3">
+              Entrar
+            </Button>
+
+            <div className="flex items-center gap-3 w-full">
+              <Separator className="shrink bg-gray-300" />
+              <span className="text-gray-500 text-sm leading-5">ou</span>
+              <Separator className="shrink bg-gray-300" />
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <span className="text-center text-gray-600 text-sm leading-5">Ainda não tem uma conta?</span>
+
+              <Button className="bg-white rounded-md p-3 border border-gray-300 text-gray-700 hover:bg-gray-300">
+                <UserRoundPlus size={16} />
+                Criar conta
+              </Button>
+            </div>
+          </form>
+
+
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
