5f7fe0ea059409754e539243e57057df94f2bd57
Augusto Moscardo <augustomsaluz@gmail.com>
2026-01-29 07:26:47 -0300

    feat(error): applied apollo error handler.

--- frontend/src/lib/apollo.ts
+++ frontend/src/lib/apollo.ts
@@ -2,10 +2,12 @@ import { useAuthStore } from "@/stores/auth";
 import {
   ApolloClient,
   ApolloLink,
+  CombinedGraphQLErrors,
+  CombinedProtocolErrors,
   HttpLink,
   InMemoryCache,
 } from "@apollo/client";
-
+import { ErrorLink } from "@apollo/client/link/error";
 import { SetContextLink } from "@apollo/client/link/context";
 
 const httpLink = new HttpLink({
@@ -23,7 +25,29 @@ const authLink = new SetContextLink((prevContext) => {
   }
 })
 
+const errorLink = new ErrorLink(({ error }) => {
+  if (CombinedGraphQLErrors.is(error)) {
+    error.errors.forEach(({ message, locations, path }) => {
+      console.error(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`);
+      if (message === "Usuário não autenticado.") {
+        const { logout } = useAuthStore.getState();
+        logout();
+      }
+    })
+  } else if (CombinedProtocolErrors.is(error)) {
+    error.errors.forEach(({ message, extensions }) =>
+      console.log(
+        `[Protocol error]: Message: ${message}, Extensions: ${JSON.stringify(
+          extensions
+        )}`
+      )
+    );
+  } else {
+    console.error(`[Network error]: ${error}`);
+  }
+})
+
 export const apolloClient = new ApolloClient({
-  link: ApolloLink.from([authLink, httpLink]),
+  link: ApolloLink.from([errorLink, authLink, httpLink]),
   cache: new InMemoryCache(),
 });
