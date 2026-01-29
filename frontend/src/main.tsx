f9dca44b0dcbcbdd83f0dfbae6341fb73fce312f
Augusto Moscardo <augustomsaluz@gmail.com>
2026-01-28 22:48:29 -0300

    chore: add Apollo Provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,16 +6,20 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import App from './App.tsx'
 
 import './index.css'
+import { ApolloProvider } from '@apollo/client/react'
+import { apolloClient } from './lib/apollo.ts'
 
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <ApolloProvider client={apolloClient}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   </StrictMode>,
 )
